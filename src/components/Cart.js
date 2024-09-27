1b3f9bd39f713b75a5e131aa817dc51fb2eee57a
hrshit7 <harshitchoudhary000012@gmail.com>
2024-09-27 11:53:24 +0530

    For now the project phase I is ended, In future i will add many functionalities to it

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -17,6 +17,7 @@ const Cart = ()=>{
             <h1 className="font-bold text-2xl py-4">Cart</h1>
             <button className="text-xl bg-black m-2 p-2 rounded-md text-stone-50" onClick={handleDispatch}> Clear Cart</button>
             <div className="w-6/12 m-auto">
+            {cartItems.length == 0 && (<h1 className="text-stone-400">The Cart is empty, please add items...</h1>) }
                 {cartItems.map((item)=> <ItemList key={item?.cart?.info?.id} item={item}/>)}
             </div>
         </div>
